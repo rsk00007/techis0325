9fe43687738c08c280a65991f8825c3ff13b27ba
rsk00007 <116616183+rsk00007@users.noreply.github.com>
2023-05-08 20:16:41 +0900

    change https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,5 +24,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
+        if(\App::environment(['Production'])){
+            \URL::forceScheme('https');
+        }
     }
 }
