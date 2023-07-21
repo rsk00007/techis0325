85372b69f83cf9653a2f69177a0d507793483406
rsk00007 <116616183+rsk00007@users.noreply.github.com>
2023-07-21 16:05:40 +0900

    2023_07_21_システムタイトル変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', '冷蔵庫の管理') }}</title>
+    <title>{{ config('app.name', '冷蔵庫管理システム') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -24,7 +24,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', '冷蔵庫の管理') }}
+                    {{ config('app.name', '冷蔵庫管理システム') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
